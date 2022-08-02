dd317f10143f4baa05aca5106728709cdf481685
Borja LoFe <im@borjalofe.com>
2022-08-02 16:25:35 +0200

    Twenty Twenty Two theme setup updated

--- themes/twentytwentytwo/install.sh
+++ themes/twentytwentytwo/install.sh
@@ -1,18 +1,20 @@
 #!/bin/sh
 
+PARENT_THEME='twentytwentytwo'
+
 ##
 # Theme install
 ##
-wp theme install twentytwentytwo --activate
+wp theme install ${PARENT_THEME} --activate
 
 ##
 # Clean outdated theme setups
 ##
 for theme in $(wp theme list --status=inactive --field=name); do
     wp theme delete $theme
-    for optionToDelete in $(wp option list --search="theme_mods_${theme}*" --field=option_name); do
+    for optionToDelete in $(wp option list --search="*${theme}*" --field=option_name); do
         wp option delete $optionToDelete;
     done
 done
 
-wp scaffold child-theme ${CHILD_THEME} --parent_theme="twentytwentytwo" --theme_name="${DOMAIN} Child" --activate
+wp scaffold child-theme ${CHILD_THEME} --parent_theme="${PARENT_THEME}" --theme_name="${DOMAIN} Child" --activate
